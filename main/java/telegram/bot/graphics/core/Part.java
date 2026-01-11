94931ca50067f92cd0ad4f909a70ec06249c1ba8
Podushka-Hrenov <krutoidebil42@gmail.com>
2026-01-11 17:22:48 +0500

    Delete main/java/telegram/bot directory

--- main/java/telegram/bot/graphics/core/Part.java
+++
@@ -1,9 +0,0 @@
-package telegram.bot.graphics.core;
-
-public class Part extends BasePart {
-    public String shape = "Block";
-
-    public MeshData meshData() {
-        return Enum.PartType.get(shape);
-    }
-}
